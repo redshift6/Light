92f55f82ff0e602c3e168c5976255f0fa31a8bca
Andrew Solski <redshift6@gmail.com>
2014-04-24 11:09:45 +0200

    2014/04/24
    Added suitable image to app, reworked on/off handling

--- src/com/andysoft/light/MainActivity.java
+++ src/com/andysoft/light/MainActivity.java
@@ -4,15 +4,20 @@ package com.andysoft.light;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
@@ -20,14 +25,14 @@ public class MainActivity extends Activity {
 	private boolean flashState;
 	private Camera camera;
 	private Parameters params;
-	Button lightButton;
+	ImageView lightButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
-		lightButton = (Button) findViewById(R.id.imgbtn);
+		lightButton = (ImageView) findViewById(R.id.imgbtn);
 		
 		hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 		if (!hasFlash) {
@@ -46,6 +51,14 @@ public class MainActivity extends Activity {
 				toggleLight();
 			}
 		});
+
+        final TextView attriblink = (TextView) findViewById(R.id.attribution);
+        attriblink.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.flaticon.com/free-icon/lightbulb_2613"));
+                startActivity(browserIntent);
+            }
+        });
 	}
 	private void getCamera() {
 		if (camera == null) {
@@ -72,15 +85,16 @@ public class MainActivity extends Activity {
 			camera.setParameters(params);
 			camera.startPreview();
 			flashState = true;
-			lightButton.setText(getString(R.string.light_on));
+			//lightButton.setText(getString(R.string.light_on));
+            lightButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_light_on));
 		} else if (flashState) { //turn off flash if it is on
 			params = camera.getParameters();
 			params.setFlashMode(Parameters.FLASH_MODE_OFF);
 			camera.setParameters(params);
 			camera.stopPreview();
 			flashState = false;
-			lightButton.setText(getString(R.string.light_off));
-			
+			//lightButton.setText(getString(R.string.light_off));
+            lightButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_light_off));
 		}
 		lightButton.setKeepScreenOn(flashState);
 	}
@@ -88,7 +102,6 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onStart() {
 	    super.onStart();
-	     
 	    // on starting the app get the camera params
 	    getCamera();
 	}
@@ -106,7 +119,6 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onStop() {
 	    super.onStop();
-	     
 	    // on stop release the camera
 	    if (camera != null) {
 	        camera.release();
